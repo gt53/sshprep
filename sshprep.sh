11f81c0b1a48d62d28271497b0d736d669f041be
outghter <oughter@eml.cc>
2016-09-10 12:37:41 -0400

    Clean up and use colors for output

--- sshprep.sh
+++ sshprep.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+TEXT_GREEN=$(tput setaf 2)
+TEXT_YELLOW=$(tput setaf 3)
+TEXT_RED=$(tput setaf 1)
+TEXT_RESET=$(tput sgr0)
+
 usage() {
   local name=$(basename $0)
   name=${name%.sh}
   cat <<EOM
-  Usage: ${name} ipAddress
+  Usage: ${name} host-name-or-ip
 EOM
   exit 0
 }
 
 # Validate param
-if [[ $# -eq 0 ]]; then
+if [ $# -eq 0 ]; then
   usage;
   exit 1
 fi
@@ -21,18 +26,18 @@ host=$1
 declare -a sshprep_files=(
   "$HOME/.bashrc"
   "$HOME/.vimrc"
-);
+)
 
 # Validate $sshprepf_files
-if [[ -z $sshprep_files ]]; then
-  echo "ERROR: 'sshprep_files' not defined"
+if [ -z $sshprep_files ]; then
+  echo "${TEXT_RED}ERROR: 'sshprep_files' not defined${TEXT_RESET}"
   exit 1
 fi
 
 # Validate all files exist
 for file in "${sshprep_files[@]}"; do
-  if [[ ! -f "${file}" ]]; then
-    echo "ERROR: ${file} not found"
+  if [ ! -f "${file}" ]; then
+    echo "${TEXT_RED}ERROR: File ${file} not found${TEXT_RESET}"
     exit 1
   fi
 done
