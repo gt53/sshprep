07c30642284f1137d5a7510f843362da6cfeb8a7
outghter <oughter@eml.cc>
2016-09-12 23:54:28 -0400

    Update to work as a script that gets sourced

--- sshprep.sh
+++ sshprep.sh
@@ -6,10 +6,8 @@ TEXT_RED=$(tput setaf 1)
 TEXT_RESET=$(tput sgr0)
 
 usage() {
-  local name=$(basename $0)
-  name=${name%.sh}
   cat <<EOM
-Usage: ${name} host-name-or-ip
+Usage: sshprep host-name-or-ip
 
 Setup: Define the array \$sshprep_files somewhere such as ~/.bashrc -- example:
   declare -a sshprep_files=(
@@ -18,36 +16,36 @@ Setup: Define the array \$sshprep_files somewhere such as ~/.bashrc -- example:
   )
 
 EOM
-  exit 0
+  return 0
 }
 
-# Validate param
-if [ $# -eq 0 ]; then
-  usage;
-  exit 1
-fi
-
-host=$1
-
-# Validate $sshprepf_files
-if [ -z $sshprep_files ]; then
-  echo "${TEXT_RED}ERROR: 'sshprep_files' not defined${TEXT_RESET}"
-  echo ""
-  usage;
-  exit 1
-fi
-
-# Warn about files that don't exist
-for file in "${sshprep_files[@]}"; do
-  if [ ! -f "${file}" ]; then
-    echo "${TEXT_YELLOW}Warning: File ${TEXT_RESET}${file}${TEXT_YELLOW} not found. Skipping ...${TEXT_RESET}"
+sshprep() {
+  # Validate host param
+  if [ $# -eq 0 ]; then
+    usage;
+    return 1
   fi
-done
 
-# Copy files
-scp ${sshprep_files[*]} $host:~
+  host=$1
 
-# Connect
-ssh -o ServerAliveInterval=60 $host
+  # Validate $sshprepf_files
+  if [ -z $sshprep_files ]; then
+    echo "${TEXT_RED}ERROR: 'sshprep_files' not defined${TEXT_RESET}"
+    echo ""
+    usage;
+    return 1
+  fi
 
-exit 0
+  # Warn about files that don't exist
+  for file in "${sshprep_files[@]}"; do
+    if [ ! -f "${file}" ]; then
+      echo "${TEXT_YELLOW}Warning: File ${TEXT_RESET}${file}${TEXT_YELLOW} not found. Skipping ...${TEXT_RESET}"
+    fi
+  done
+
+  # Copy files
+  scp ${sshprep_files[*]} $host:~
+
+  # Connect
+  ssh -o ServerAliveInterval=60 $host
+}
